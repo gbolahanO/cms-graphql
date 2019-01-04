917bf9a7fc7cb9bf58c04e5e6b29d7b024669994
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2019-01-04 18:47:51 +0100

    create user mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs'
 import User from '../models/User'
 import Post from '../models/Post'
 const Mutation = {
@@ -5,8 +6,18 @@ const Mutation = {
     let post = await Post.create(data);
     return post;
   },
-  createUser: async (_, { data }, ctx, info) => {
-    let user = await User.create(data);
+  createUser: async (_, args, ctx, info) => {
+    const { firstname, lastname, email, password } = args.data
+    
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    let user = await User.create({
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword
+    });
     return user;
   }
 }
